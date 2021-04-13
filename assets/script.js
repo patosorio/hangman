f9832025fb6cad78106edcad9e31ae8bfe9b9c31
patOS <patriciaosorio194@gmail.com>
2021-04-13 14:27:40 +0000

    checkLetter fix bugs / game working

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 const baseURL = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=";
 const baseURL_b = "&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=2&maxLength=9&api_key=qruoq28yifqp8eyc2aj97iwlvidotscjjgwmyl3n3v1ffeyfz";
 let wordToGuess = [];
-let count = 0;
+
+
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
@@ -45,21 +46,40 @@ function runGame(gameType) {
 }
 
 function checkLetter() {
-    let userLetter = document.getElementById("letter-box").value;
+    var userLetter = $("#letter-box").val();
+
     let getWord = localStorage.getItem("word"); // string
-    let updatedWord = getWord.split(''); // object
+    let correctWord = getWord.split(''); // object
+    console.log(correctWord); // ["e", "x", "a", "m", "p", "l", "e"];
 
-    for (let c of updatedWord) {
-
+    for (let c of correctWord) {
         if (c == userLetter) {
-            console.log(c);
+            index = correctWord.multiIndexOf(c);
+            for (let x of index) {
+                console.log(x);
+                wordToGuess.splice(x, 1, c);
+            }
         } else {
-
+            updateScore();
         }
     }
     updateGame(wordToGuess);
 }
 
+// function to find indexes for each correct user letter from the correct answer
+// https://stackoverflow.com/questions/24241462/how-to-search-for-multiple-indexes-of-same-values-in-javascript-array
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+
+
+Array.prototype.multiIndexOf = function (el) {
+    var idxs = [];
+    for (var i = this.length - 1; i >= 0; i--) {
+        if (this[i] === el) {
+            idxs.unshift(i);
+        }
+    }
+    return idxs;
+};
 
 function updateGame(wordToGuess) {
     document.getElementById("wordToGuess").innerHTML = wordToGuess.join(' ');
